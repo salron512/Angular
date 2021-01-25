767219ec98b43976d8c157e179bf98f3a380ca44
salron512 <73781657+salron512@users.noreply.github.com>
2021-01-25 13:48:42 -0400

    commit

--- cursoAngular/CRUD/frontend/src/app/app-routing.module.ts
+++ cursoAngular/CRUD/frontend/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './views/home/home.component';
 import {ProductCrudComponent} from './views/product-crud/product-crud.component';
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
 const routes: Routes = [
   {
   path: "",
@@ -20,6 +21,10 @@ const routes: Routes = [
 {
   path: "products/update/:id",
   component: ProductUpdateComponent
+},
+{
+  path: "products/delete/:id",
+  component: ProductDeleteComponent
 }
 ];
 
